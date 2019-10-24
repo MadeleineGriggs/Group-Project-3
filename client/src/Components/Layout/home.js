3d19e90f48d292a2b01c0b1caf204ee72b15bebf
Madeleine Griggs <madeleinegriggs@gmail.com>
2019-10-24 17:06:56 -0400

    added Material-UI to create-react-app

--- client/src/Components/Layout/home.js
+++ client/src/Components/Layout/home.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "./home.css";
+import Button from '@material-ui/core/Button';
 
 const Home = props => (
   <div className="homeContainer">
    <p>The Home container is here.</p>
+   <Button variant="contained" color="primary">
+      Hello World
+    </Button>
   </div>
 );
 export default Home;
