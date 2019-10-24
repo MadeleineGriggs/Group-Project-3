3d4560b5a4f6cf9dc8dc7c861fd0b4ca935d0602
Madeleine Griggs <madeleinegriggs@gmail.com>
2019-10-24 16:01:20 -0400

    added express server with create react app and hooked together.

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './Components/Layout/home.js';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {    
+    super(props);    
+    this.state = {}
+    this.connecToServer = this.connecToServer.bind(this);
+  }
+
+  connecToServer() {    
+    fetch('/');  
+  }
+
+  componentDidMount() {    
+    this.connecToServer();  
+  }
+  render() {
+    return (
+      <Router>
+        <Route exact path="/" component={Home} /> 
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/Components/Layout/home.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./home.css";
+
+const Home = props => (
+  <div className="homeContainer">
+   <p>The Home container is here.</p>
+  </div>
+);
+export default Home;
