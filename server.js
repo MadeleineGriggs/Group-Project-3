99cd133e6c9167b98e67f0f9d6e9e6992c7a1fba
Madeleine G <madeleinegriggs@gmail.com>
2019-11-05 18:57:51 -0500

    Changed = to + in build and production app.get * route for res.sendFile.

--- server.js
+++ server.js
@@ -32,13 +32,13 @@ require("./routes/apiRoutes")(app);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
   app.get("*", (req, res) => {
-    res.sendfile(path.join((__dirname + "client/build/index.html")));
+    res.sendfile(path.join((__dirname = "client/build/index.html")));
   });
 }
 
 //build mode
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/public/index.html"));
+  res.sendFile(path.join(__dirname = "/client/public/index.html"));
 });
 
 
