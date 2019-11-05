6acb8d005754011aab27a78a141c8c80db7098f1
Madeleine G <madeleinegriggs@gmail.com>
2019-11-05 16:45:53 -0500

    Change prod route to / from *

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ var syncOptions = { force: false };
 // production mode
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
-  app.get("*", (req, res) => {
+  app.get("/", (req, res) => {
     res.sendfile(path.join((__dirname = "client/build/index.html")));
   });
 }
